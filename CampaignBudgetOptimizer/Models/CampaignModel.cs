8d48a95b0de1826ef91ecf4932a36a4f20a289eb
al16g20 <al16g20@soton.ac.uk>
2024-08-21 19:50:15 +0300

    first commit

---
+++ CampaignBudgetOptimizer/Models/CampaignModel.cs
@@ -0,0 +1,13 @@
+namespace CampaignBudgetOptimizer.Models
+{
+    public class CampaignModel
+    {
+        public double TotalAdSpend { get; set; }
+        public double Ad1Budget { get; set; }
+        public double Ad2Budget { get; set; }
+        public double Ad3Budget { get; set; }
+        public double AgencyFeePercentage { get; set; }
+        public double ToolFeePercentage { get; set; }
+        public double AgencyHours { get; set; }
+    }
+}
