60652b4a7e540b7ab8d901d339cd2e72c10ff31a
al16g20 <al16g20@soton.ac.uk>
2024-08-25 16:16:26 +0300

    make properties nullable

--- CampaignBudgetOptimizer/Pages/Index.cshtml.cs
+++ CampaignBudgetOptimizer/Pages/Index.cshtml.cs
@@ -1,19 +1,43 @@
+using CampaignBudgetOptimizer.Models;
+using CampaignBudgetOptimizer.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace CampaignBudgetOptimizer.Pages;
-
-public class IndexModel : PageModel
+namespace CampaignBudgetOptimizer.Pages
 {
-    private readonly ILogger<IndexModel> _logger;
-
-    public IndexModel(ILogger<IndexModel> logger)
+    public class IndexModel : PageModel
     {
-        _logger = logger;
-    }
+        private readonly BudgetCalculator _budgetCalculator;
 
-    public void OnGet()
-    {
+        public IndexModel(BudgetCalculator budgetCalculator)
+        {
+            _budgetCalculator = budgetCalculator;
+        }
 
+        [BindProperty]
+        public CampaignModel? Campaign { get; set; } // Note the nullable type
+
+        [BindProperty]
+        public string? AdToOptimize { get; set; } // Note the nullable type
+
+        public void OnGet()
+        {
+            // Initialize default values if needed
+            Campaign = new CampaignModel(); // Initialize Campaign
+        }
+
+        public void OnPost()
+        {
+            if (Campaign != null && AdToOptimize != null)
+            {
+                double optimalBudget = _budgetCalculator.CalculateOptimalAdBudget(Campaign, AdToOptimize);
+                ViewData["OptimalBudget"] = optimalBudget;
+            }
+            else
+            {
+                // Handle the case where Campaign or AdToOptimize is null
+                ViewData["OptimalBudget"] = "Error: Please provide all required fields.";
+            }
+        }
     }
 }
--- CampaignBudgetOptimizer/obj/Debug/net8.0/CampaignBudgetOptimizer.AssemblyInfo.cs
+++ CampaignBudgetOptimizer/obj/Debug/net8.0/CampaignBudgetOptimizer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CampaignBudgetOptimizer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d48a95b0de1826ef91ecf4932a36a4f20a289eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5005bd4c1da42b2babfb7a39936ecf4b012cbdf8")]
 [assembly: System.Reflection.AssemblyProductAttribute("CampaignBudgetOptimizer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CampaignBudgetOptimizer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
